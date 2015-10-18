e67e87e35ea814c58e9422411981d9eb126f3669
Ondrej Synacek <ondrej.synacek@gmail.com>
2015-10-18 17:40:51 +0200

    shopping cart iteration 2

--- Week1/2015-10-15/shopping_cart.rb
+++ Week1/2015-10-15/shopping_cart.rb
@@ -13,11 +13,11 @@ class ShoppingCart
   end
 
   def show
-    calculate
     quantities
-    #binding.pry
+    discounts
+    calculate
     @qtities.each do |name, qty|
-      puts "##{qty}  #{name} #{@sum[name]}"
+      puts "#{qty.to_s.colorize( :background => :white, :color => :black)}  #{name}(s) #{@sum[name].to_s.colorize(:red)}"
     end
   end
 
@@ -29,6 +29,34 @@ class ShoppingCart
         @sum[item.name] = item.price
       end
     end
+  end
+
+  def discounts
+    @qtities.each do |name, qty|
+      # every second apple is for free
+      if name == 'apple' && qty > 1
+        apples = @items.select do |item|
+          item.name == 'apple'
+        end
+        apples[0..apples.size/2].each do |apple|
+          apple.price = 0
+        end
+      # every third orange is free
+      elsif name == 'orange' && qty > 2 
+        oranges = @items.select do |item|
+          item.name == 'orange'
+        end
+        oranges[0..oranges.size/3].each do |orange|
+          orange.price = 0
+        end
+      elsif name == 'grape' && qty == 4
+        free_banana = Banana.new
+        free_banana.price = 0
+        @qtities['banana'] += 1
+        @items << free_banana
+      end
+    end
+
   end
 
   def quantities
@@ -43,7 +71,7 @@ class ShoppingCart
 end
 
 class Item
-  attr_reader :name, :price
+  attr_accessor :name, :price
   def initialize(name, price)
     @name = name
     @price = price 
@@ -62,13 +90,30 @@ class Banana < Item
   end
 end
 
-cart = ShoppingCart.new
-an_apple = Apple.new
-cart.add_item(an_apple)
+class Orange < Item
+  def initialize
+    super('orange', 5)
+  end
+end
 
+class Grape < Item
+  def initialize
+    super('grape', 15)
+  end
+end
+
+cart = ShoppingCart.new
+
+4.times do
+  cart.add_item(Apple.new)
+end
 3.times do 
   cart.add_item(Banana.new)
 end
-
+7.times do
+  cart.add_item(Orange.new)
+end
+4.times do
+  cart.add_item(Grape.new)
+end
 cart.show
-
