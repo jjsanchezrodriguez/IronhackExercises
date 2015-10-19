57bfc1e4abbacaaf179947946bd0e48ea0398ec8
Ondrej Synacek <ondrej.synacek@gmail.com>
2015-10-19 19:31:46 +0200

    Sinatra to-do list unit tests & skeleton (Monday)

---
+++ Week2/2015-10-19/Sinatra-to-do-list/todo_task_tests.rb
@@ -0,0 +1,43 @@
+require 'rspec'
+require './todo_task'
+require 'date'
+
+RSpec.describe "Task Class tests" do
+
+  before :each do  
+    @task = Task.new('test text')
+  end
+
+  it "task counter should be 2 when creating two objects" do
+    second_task = Task.new('blablabla')
+    expect(second_task.id).to eq(2)
+  end
+
+  
+  it "marking task as complete should return 'true' when calling completed?" do
+    expect(@task.complete).to eq(false)
+    @task.complete!
+    expect(@task.completed?).to eq(true)
+  end
+
+  it "when calling make_incomplete!, the method completed? should return false" do
+    @task.make_incomplete!
+    expect(@task.completed?).to eq(false)
+  end
+
+  it "when initizialing a task, the created_at attribute == current time" do
+    expect(@task.created_at).to be_within(0.002).of(Time.now)
+  end
+
+  it "update_content! overwrites content attribute with given argument" do
+    @task.update_content!("new content")
+    expect(@task.content).to eq("new content")
+  end
+
+  it "update_content! sets attribute update_at to the time of change" do
+    @task.update_content!("changed again")
+    expect(@task.content).to eq("changed again")
+    expect(@task.updated_at).to be_within(0.002).of(Time.now)
+  end
+
+end
---
+++ Week2/2015-10-19/Sinatra-to-do-list/todo_todo.rb
@@ -0,0 +1,46 @@
+require 'pry'
+require 'yaml/store'
+
+class ToDo
+  attr_reader :tasks, :user
+  def initialize(user)
+    @tasks = []
+    @user = user
+    @todo_store = YAML::Store.new("tasks.yml")
+  end
+
+  def add_task(task)
+    @tasks << task
+  end
+
+  def delete_task!(del_id)
+    @tasks.delete_if {|task| task.id == del_id}
+  end
+
+  def find_by_id(search_id)
+    @tasks.find { |task| task.id == search_id }
+  end
+
+  def sort_by_created(mode)
+    if mode == "ASC"
+      @tasks.sort do |task1, task2|
+        task1.created_at <=> task2.created_at
+      end
+    elsif mode == "DESC"
+      @tasks.sort do |task1, task2|
+        task2.created_at <=> task1.created_at
+      end
+    end
+  end
+
+  def save
+    @todo_store.transaction do
+      @todo_store[@user] = @tasks
+    end
+  end
+
+  def load_tasks
+    @tasks = []
+    @tasks = YAML::load_file('tasks.yml')
+  end
+end
