83d913f9fc422bb8c7ac188062af29f9c84c1049
Ondrej Synacek <ondrej.synacek@gmail.com>
2015-11-06 18:01:24 +0100

    Auction site refactoring

--- Week4/2015-11-05/AuctionSite/app/controllers/bids_controller.rb
+++ Week4/2015-11-05/AuctionSite/app/controllers/bids_controller.rb
@@ -14,27 +14,5 @@ class BidsController < ApplicationController
       flash[:alert] = "Bid successful."
       redirect_to user_product_path(params[:user_id], params[:product_id])
     end 
-
-
-    #if user.nil?
-      #flash[:error] = "User does not exist." 
-      #redirect_to user_product_path(params[:user_id], params[:product_id])
-    #elsif user.email == product.user.email
-      #flash[:error] = "You cannot bid on your items."
-      #redirect_to user_product_path(params[:user_id], params[:product_id])
-    #else
-      #bid = Bid.new(amount: params[:bid][:amount], user_id: user.id, product_id: params[:product_id])
-
-      #if params[:bid][:amount].to_f < product.minimum_bid
-        #flash[:error] = "Bid too low." 
-        #redirect_to user_product_path(params[:user_id], params[:product_id])
-      #elsif bid.save
-        #flash[:alert] = "Bid successful."
-        #redirect_to user_product_path(params[:user_id], params[:product_id])
-      #else
-        #flash[:error] = bid.errors.full_messages
-        #redirect_to user_product_path(params[:user_id], params[:product_id])
-      #end 
-    #end
   end
 end
